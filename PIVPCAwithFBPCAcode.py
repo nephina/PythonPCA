c14e75409057663a4ae652c5c2fef3920a000797
nephina <millerat@uwm.edu>
2018-08-17 19:31:23 -0500

    changed floating point images intake to uint8

--- PIVPCAwithFBPCAcode.py
+++ PIVPCAwithFBPCAcode.py
@@ -9,13 +9,16 @@ import os
 import fnmatch
 
 
-NumOfSubtractionModes = 1
-NumOfImagesPerSVDCycle = 250
+NumOfSubtractionModes = 6
+NumOfImagesPerSVDCycle = 450
+
+
+def uint8load(f):
+	return (255*io.imread(f,as_gray=True)).astype(np.uint8)
 
 Frames = ['*V3VLA*','*V3VLB*','*V3VRA*','*V3VRB*','*V3VTA*','*V3VTB*']
 FileNames = sorted(os.listdir('./RawData'))
 NumOfCycles = (len(FileNames)/(NumOfImagesPerSVDCycle*6))
-
 CycleNumber = 1
 while CycleNumber <= NumOfCycles: # Cycle through all the files in NumOfImagesPerSVDCycle increments
 	CycleNames = FileNames[(((NumOfImagesPerSVDCycle*6)*CycleNumber)-(6*NumOfImagesPerSVDCycle)):((NumOfImagesPerSVDCycle*6)*CycleNumber)] #all the names for one cycle, read in 1LA,1LB,1RA,1RB,1TA,1TB,2LA,etc.
@@ -23,13 +26,12 @@ while CycleNumber <= NumOfCycles: # Cycle through all the files in NumOfImagesPe
 		print("\n--------------------------Matching Names---------------------------\n")
 		FrameNames = fnmatch.filter(CycleNames,Frames[frame]) #find all the names for a given frame in the cycle
 		NumberofFiles = len(FrameNames)
-
 		ImageSize = np.shape(io.imread("./RawData/"+FrameNames[0],as_gray=True)) #test the first image for size
-		ImageVectors = np.empty([NumberofFiles,ImageSize[0]*ImageSize[1]]) #initialize the matrix for SVD
+		ImageVectors = np.empty([NumberofFiles,ImageSize[0]*ImageSize[1]],dtype=np.uint8) #initialize the matrix for SVD
 		print("\n--------------------------Reading Images---------------------------\n")
-		ImageCollection = io.concatenate_images(io.ImageCollection(["./RawData/"+x for x in FrameNames],as_gray=True)) #read all images in a given cycle and frame at one time
+		ImageCollection = io.concatenate_images(io.ImageCollection(["./RawData/"+x for x in FrameNames],load_func=uint8load)) #read all images in a given cycle and frame at one time
 		for ImageNumber in range(NumberofFiles):
-			ImageVectors[ImageNumber,:] = np.ravel(ImageCollection[ImageNumber,:,:],"F") #ravel/vectorize each 2D image to 1D and insert into the 2D stack "ImageVectors"
+			ImageVectors[ImageNumber,:] = (np.ravel(ImageCollection[ImageNumber,:,:],"F")).astype(np.uint8) #ravel/vectorize each 2D image to 1D and insert into the 2D stack "ImageVectors"
 			print("Vectorizing Image: ",FrameNames[ImageNumber])
 		del ImageCollection #memory management
 		print("\n-----------------Performing Sparse SVD Algorithm-------------------\n")
