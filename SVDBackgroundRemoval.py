5f3609e6889fc4790f02b544233ffa17fced1d1f
nephina <millerat@uwm.edu>
2018-08-29 16:08:27 -0500

    switched to opencv to read/write images, much faster than scikit-image

--- SVDBackgroundRemoval.py
+++ SVDBackgroundRemoval.py
@@ -3,24 +3,22 @@ import scipy as sc
 from scipy.sparse.linalg import svds
 from scipy.sparse import csr_matrix
 from scipy import misc
-from skimage import io
 import os
 import fnmatch
 import time
+import cv2
 
 #Put this python script in the run folder, and create a folder in the run folder called "Processed," it will read from "RawData" and write into "Processed"
 
 
 #Input Variables
-NumOfSubtractionModes = 3 #more modes means more time computing the SVD as well as doing background subtraction for each image
+NumOfSubtractionModes = 2 #more modes means more time computing the SVD as well as doing background subtraction for each image
 NumOfImagesPerSVDCycle = 601 #must be an exact divisor of the total number of images in each frame(i.e. the run was 1500 captures, use 100, 150, 300, 500, etc.)
-
-
-def uint8load(f):
-	return ((255 / (np.max(io.imread(f,as_gray=True)))) * io.imread(f,as_gray=True)).astype(np.uint8) #import each image file as a matrix of bytes rather than floats, and scale it to exactly fit in a 0-255 range
-os.system('cls' if os.name == 'nt' else 'clear')
 Frames = ['*V3VLA*','*V3VLB*','*V3VRA*','*V3VRB*','*V3VTA*','*V3VTB*'] #Frame name keyphrases that it searches for
-NumOfFrames = 6
+
+
+os.system('cls' if os.name == 'nt' else 'clear')
+NumOfFrames = len(Frames)
 FileNames = sorted(os.listdir('./RawData'))
 NumOfCycles = (len(FileNames)/(NumOfImagesPerSVDCycle*NumOfFrames))
 for CycleNumber in range(1,(np.floor(NumOfCycles)).astype(int) + 1): # Cycle through all the files in NumOfImagesPerSVDCycle increments, floor for odd numbers of files that don't match multiples of NumImagesPerSVDCycle
@@ -28,13 +26,14 @@ for CycleNumber in range(1,(np.floor(NumOfCycles)).astype(int) + 1): # Cycle thr
 	for frame in range(NumOfFrames): # Cycle through all the frames in a given cycle
 		FrameNames = fnmatch.filter(CycleNames,Frames[frame]) #find all the names for a given frame in the cycle
 		NumberofFiles = len(FrameNames)
-		ImageSize = np.shape(io.imread("./RawData/" + FrameNames[0],as_gray=True)) #test the first image for size
+		ImageSize = np.shape(cv2.imread("./RawData/" + FrameNames[0])) #test the first image for size
+		ImageSize =ImageSize[0:2]
 		ImageVectors = np.empty([NumberofFiles,ImageSize[0] * ImageSize[1]],dtype=np.uint8) #loop initialization
 		print("\n--------------------------Reading Images---------------------------\n")
-		ImageCollection = io.concatenate_images(io.ImageCollection(["./RawData/"+x for x in FrameNames],load_func=uint8load)) #read all images in a given cycle and frame at one time, reading them in byte format
 		for ImageNumber in range(NumberofFiles):
-			ImageVectors[ImageNumber,:] = (np.ravel(ImageCollection[ImageNumber,:,:],"F")).astype(np.uint8) #ravel/vectorize each 2D image to 1D and insert into the 2D stack "ImageVectors"
-		del ImageCollection #memory management
+			print("Reading:",FrameNames[ImageNumber])
+			image = cv2.imread("./RawData/"+FrameNames[ImageNumber]).astype(np.uint8)
+			ImageVectors[ImageNumber,:] = (np.ravel(image[:,:,0],"F")) #ravel/vectorize each 2D image to 1D and insert into the 2D stack "ImageVectors"
 		print("\n-----------------Performing Sparse SVD Algorithm-------------------\n")
 		u, s, v = svds(csr_matrix.asfptype(np.transpose(ImageVectors)),k=NumOfSubtractionModes,which='LM')
 		um,sm,vm = np.asmatrix(u),np.asmatrix(s),np.asmatrix(v) #move to matrix form for processing
@@ -45,11 +44,11 @@ for CycleNumber in range(1,(np.floor(NumOfCycles)).astype(int) + 1): # Cycle thr
 			for modes in range(NumOfSubtractionModes):
 				SingleImageModes[modes,:,:] = np.reshape(um[:,modes] * sm[:,modes] * vm[modes,image],[ImageSize[0],ImageSize[1]],order="F") #reshape the modes into a stack of 2D background images
 			FilteredImage = np.reshape(np.transpose(ImageVectors[image,:]),[ImageSize[0],ImageSize[1]],order="F") - np.sum(SingleImageModes,axis=0) #subtract the sum of all background modes from the original image
-			STDFilter = 7*np.std(FilteredImage)
-			FilteredImage[FilteredImage > STDFilter] = STDFilter
 			FilteredImage[FilteredImage < 0] = 0 #clean up/delete all negative values, as those will wrap around when saving it in byte format
+			STDFilter = 10*np.std(FilteredImage[FilteredImage != 0])
+			FilteredImage[FilteredImage > STDFilter] = STDFilter
 			print("Writing:",FrameNames[image])
-			misc.imsave('./Processed/PCA'+FrameNames[image],misc.bytescale(FilteredImage)) #save the image in byte format
+			cv2.imwrite('./Processed/PCA'+FrameNames[image],misc.bytescale(FilteredImage)) #save the image in byte format
 		timeleft = (time.clock() * ((np.floor(NumOfCycles)*NumOfFrames) / ( ((CycleNumber-1)*NumOfFrames)+(frame+1) )))-time.clock()
 		os.system('cls' if os.name == 'nt' else 'clear')
 		print("\nTime to completion:",format(timeleft/60, '.4f'),"minutes")
